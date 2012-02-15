c2a57aab7418ffb4037195aa3f2c1743c58adc32
zemanta <zemanta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-15 12:11:21 +0000

    1.0.5 release tag missing views
    git-svn-id: https://plugins.svn.wordpress.org/zemanta/tags/1.0.5@505362 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/options-input-checkbox.php
@@ -0,0 +1,10 @@
+
+<input id="zemanta_options_<?php echo $field; ?>" name="zemanta_options[<?php echo $field; ?>]" type="checkbox" value="1" <?php echo isset($option) && !empty($option) ? 'checked="checked"' : ''; ?> />
+
+<?php if (isset($description)): ?>
+
+  <label for="zemanta_options_<?php echo $field; ?>">
+    <?php echo $description; ?>
+  </label>
+
+<?php endif; ?>
