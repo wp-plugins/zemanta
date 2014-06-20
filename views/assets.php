0a0625c1cbcf2f0585d5e0bf399145ea1d40a74d
ddksr <ddksr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-20 08:53:57 +0000

    * HTTPS dashboard support * Moved all links to plugin settings under Settings TAG!
    git-svn-id: https://plugins.svn.wordpress.org/zemanta/tags/1.2.5@935580 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/assets.php
+++ views/assets.php
@@ -27,4 +27,4 @@ endfor;
 //]]>
 </script>
 
-<script type="text/javascript" id="zemanta-loader" src="http://fstatic.zemanta.com/plugins/wordpress/loader.js"></script>
+<script type="text/javascript" id="zemanta-loader" src="//s3.amazonaws.com/static.zemanta.com/plugins/wordpress/loader.js"></script>
